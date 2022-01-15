9d0b54f3158e1ea5d5e4cf903fbd42c723ec0831
Luis Sousa <lv-ss@hotmail.com>
2022-01-15 14:28:29 -0300

    change module to calendar module

--- test/dekatrian_test.exs
+++ test/dekatrian_test.exs
@@ -1,32 +1,4 @@
 defmodule DekatrianTest do
   use ExUnit.Case
-  doctest Dekatrian
-
-  test "Convert from gregorian" do
-    gregorian_date = %Date{year: 2000, month: 4, day: 28}
-    dekatrian_date = %Dekatrian{year: 2000, month: 5, day: 05}
-
-    assert Dekatrian.from_gregorian(gregorian_date) == dekatrian_date
-  end
-
-  test "Acrhonian" do
-    gregorian_date = %Date{year: 2022, month: 1, day: 1}
-    dekatrian_date = %Dekatrian{year: 2022, month: 0, day: "Achronian"}
-
-    assert Dekatrian.from_gregorian(gregorian_date) == dekatrian_date
-  end
-
-  test "Sincronian" do
-    gregorian_date = %Date{year: 2020, month: 1, day: 2}
-    dekatrian_date = %Dekatrian{year: 2020, month: 0, day: "Sincronian"}
-
-    assert Dekatrian.from_gregorian(gregorian_date) == dekatrian_date
-  end
-
-  test "Acrhonian in leap year" do
-    gregorian_date = %Date{year: 2020, month: 1, day: 1}
-    dekatrian_date = %Dekatrian{year: 2020, month: 0, day: "Achronian"}
-
-    assert Dekatrian.from_gregorian(gregorian_date) == dekatrian_date
-  end
+  doctest Calendar.Dekatrian
 end
